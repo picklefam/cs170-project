3bd0773cb249e1db22c7a5651c87b9a2e99c723d
Jonathan Holobetz <jonoholobetz@gmail.com>
2020-05-02 20:52:31 -0700

    Naive solver?

--- Phase2/max_st.py
+++ Phase2/max_st.py
@@ -1,4 +1,5 @@
 from parse import *
+from solver import solve
 import networkx as nx
 import os
 
@@ -8,5 +9,5 @@ if __name__ == "__main__":
     for input_path in os.listdir(input_dir):
         graph_name = input_path.split(".")[0]
         G = read_input_file(f"{input_dir}/{input_path}")
-        T = nx.maximum_spanning_tree(G)
+        T = solve(G)
         write_output_file(T, f"{output_dir}/{graph_name}.out")
---
+++ Phase2/solver.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+import networkx as nx
+from parse import read_input_file, write_output_file
+from utils import is_valid_network, average_pairwise_distance
+import sys
+from itertools import chain, combinations
+
+
+# In[24]:
+
+
+def solve(G):
+    """
+    Args:
+        G: networkx.Graph
+    Returns:
+        T: networkx.Graph
+    """
+    # TODO: your code here!
+    T = nx.minimum_spanning_tree(G)
+    apd = average_pairwise_distance_fast(T)
+    while not T.is_empty():
+        flag = False
+        leaves = [n for n, d in T.degree() if d == 1]
+        for leaf in leaves:
+            subtree = T.copy()
+            subtree.remove_node(leaf)
+            new_apd = average_pairwise_distance_fast(subtree)
+            if is_valid_network(subtree) and new_apd < apd:
+                T = subtree
+                apd = new_apd
+                flag = True
+        # flag = False => no better tree available
+    if not flag:
+        return T
+
+
+# In[25]:
+
+
+# def powerset(iterable):
+#     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+#     s = list(iterable)
+#     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+
+# Here's an example of how to run your solver.
+# Usage: python3 solver.py test.in
+
+# In[10]:
+
+
+if __name__ == '__main__':
+    assert len(sys.argv) == 2
+    path = sys.argv[1]
+    G = read_input_file(path)
+    T = solve(G)
+    assert is_valid_network(G, T)
+    print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
+    write_output_file(T, 'out/test.out')
+
