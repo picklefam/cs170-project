ed6b11116b00a38394fc33b5e59f4cc08a061c62
Jonathan Holobetz <jonoholobetz@gmail.com>
2020-05-03 23:05:24 -0700

    Completed submission

--- Phase2/solver.py
+++ Phase2/solver.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[84]:
+# In[1]:
 
 
 import networkx as nx
 import numpy as np
+import sys
 from parse import read_input_file, write_output_file
 from utils import is_valid_network, average_pairwise_distance, average_pairwise_distance_fast
-import sys
 from itertools import chain, combinations
 
 
-# In[108]:
+# In[2]:
 
 
 def solve(G):
@@ -28,16 +28,12 @@ def solve(G):
     except ZeroDivisionError:
         pass
     high_degree = sorted(G.degree, key=lambda x: x[1], reverse=True)[0][0]
-    #high_degree = [tup[0] for tup in high_degree]
-    #start_maxd = min(high_degree, key=lambda n: G.degree(n, weight='weight') / G.degree(n))
     S_3 = generate_spt(G, high_degree)
     trees += [network(G, S_3, "DSPT")]
-    #S_4 = random_network(G)
-    #trees += [S_4]
     return min(trees, key=lambda t: average_pairwise_distance_fast(t))
 
 
-# In[109]:
+# In[3]:
 
 
 def generate_spt(G, start):
@@ -50,7 +46,7 @@ def generate_spt(G, start):
     return spt
 
 
-# In[110]:
+# In[4]:
 
 
 def random_network(G):
@@ -63,7 +59,6 @@ def random_network(G):
             potential_network = T.copy()
             potential_network.add_edge(u, v, weight=w)
             p = min(1, np.exp(-1 * w / avg_edge_weight))
-#             print(p, w)
             if np.random.rand() < p:
                 try:
                     nx.find_cycle(potential_network, orientation='ignore')
@@ -78,47 +73,7 @@ def random_network(G):
     return T
 
 
-# In[118]:
-
-
-# def picky_network(G, T, name):
-#     """
-#     Args:
-#         G: networkx.Graph
-#     Returns:
-#         T: networkx.Graph
-#     """
-#     # TODO: your code here!
-#     current_best = T
-#     current_apd = average_pairwise_distance_fast(current_best)
-#     avg_edge_weight = G.size(weight='weight') / G.size()
-#     unvisited = [True] * nx.number_of_nodes(G)
-#     while nx.number_of_nodes(T) > 1 and any(unvisited):
-#         flag = False
-#         leaves = [n for n, d in T.degree() if d == 1]
-#         current_best = T
-#         current_apd = average_pairwise_distance_fast(current_best)
-#         for leaf in leaves:
-#             subtree = T.copy()
-#             leaf_edge_weight = T.degree(leaf, weight='weight')
-#             if leaf_edge_weight >= avg_edge_weight / 20:
-#                 subtree.remove_node(leaf)
-#                 new_apd = average_pairwise_distance_fast(subtree)
-#                 if is_valid_network(G, subtree) and new_apd < current_apd:
-#                     current_best = subtree
-#                     current_apd = new_apd
-#                     flag = True
-#             unvisited[leaf] = False
-#         T = current_best
-#         # flag = False => no better tree available
-#         if not flag:
-#             print(name + ":", current_apd)
-#             return T
-#     print(name + ":", current_apd)
-#     return T
-
-
-# In[83]:
+# In[5]:
 
 
 def network(G, T, name):
